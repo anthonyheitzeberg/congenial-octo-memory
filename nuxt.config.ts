8847483744b6da12d2dbdd3c573f441e3a52d9b3
Anthony Heitzeberg <a.heitzeberg007@gmail.com>
2024-10-16 16:02:12 -0500

    feat: add nuxt ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,33 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  modules: [
+    '@formkit/auto-animate/nuxt',
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/scripts',
+    '@nuxt/icon',
+    '@nuxt/ui',
+    '@pinia/nuxt',
+    '@vueuse/nuxt',
+    'pinia-plugin-persistedstate/nuxt',
+    '@nuxtjs/robots',
+  ],
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/scripts'],
 
+  colorMode: {
+    preference: 'light',
+  },
+  compatibilityDate: '2024-04-03',
+  typescript: {
+    typeCheck: true,
+  },
   eslint: {
     config: {
-      stylistic: true
-    }
+      stylistic: true,
+    },
   },
-  typescript: {
-    typeCheck: true
-  }
+  tailwindcss: {
+    exposeConfig: true,
+    viewer: true,
+  },
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,40 @@
+<script setup lang="ts">
+const isOpen = ref(false)
+</script>
+
 <template>
-    <div>
-      <h1>Welcome to the homepage</h1>
-    </div>
-  </template>
-  
+  <div>
+    <UButton
+      label="Open"
+      @click="isOpen = true"
+    />
+
+    <USlideover v-model="isOpen">
+      <UCard
+        class="flex flex-col flex-1"
+        :ui="{ body: { base: 'flex-1' }, ring: '', divide: 'divide-y divide-gray-100 dark:divide-gray-800' }"
+      >
+        <template #header>
+          <UButton
+            color="gray"
+            variant="ghost"
+            size="sm"
+            icon="i-heroicons-x-mark-20-solid"
+            class="flex sm:hidden absolute end-5 top-5 z-10"
+            square
+            padded
+            @click="isOpen = false"
+          />
+
+          <PlaceholderComponent class="h-8" />
+        </template>
+
+        <PlaceholderComponent class="h-full" />
+
+        <template #footer>
+          <PlaceholderComponent class="h-8" />
+        </template>
+      </UCard>
+    </USlideover>
+  </div>
+</template>
