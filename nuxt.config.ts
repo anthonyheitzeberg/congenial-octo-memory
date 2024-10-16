087be28ab83a96385d7a2455a613028cf3c89ba3
Anthony Heitzeberg <a.heitzeberg007@gmail.com>
2024-10-16 13:06:58 -0500

    feat: edit eslint option

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,12 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ['@nuxt/eslint'],
+
+  eslint: {
+    config: {
+      stylistic: true
+    }
+  },
   typescript: {
     typeCheck: true
   }
